ccd3fcb89c1f826337c044760eda450308b19837
rs119574 <rs119574@gmail.com>
2018-09-02 23:08:02 +0530

    Go n rock

--- format.cpp
+++ format.cpp
@@ -7,37 +7,40 @@ by:-
                     
 
 */ 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+using namespace std;
 #define ll long long int
 
 #define pb push_back
 #define len(a) a.size()
-#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ignor cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
-#define flush fflush(stdin);fflush(stdout);
-#define rep(i,n) for(int i=0;i<n;i++)
 #define sq(a) (a)*(a)
 #define cb(a) (a)*(a)*(a)
 #define gcd(a,b) __gcd(a,b)
 #define cil(a,b) (((a-1)/b)+1)
-#define settedbits(a) __builtin_popcount(a)
 #define totalbits(a) ((int)log2(a)+1)
+#define settedbits(a) __builtin_popcount(a)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ignor cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define flush fflush(stdin);fflush(stdout);
+#define rep(i,n) for(int i=0;i<n;i++)
 #define sp " "
+#define nl "\n"
 #define in(a) ll a;scanf("%lld",&a);
 #define out(a) printf("%lld",a);
-#define nl printf("\n");
+#define ptf printf
+
+
 
 const ll mod=1e9+7;
 const double pi=acos(-1);
+#define inf 0x3f3f3f3f
 
 int main(){
-	fast;
-	int t;cin>>t;
-	while(t--){
-
-
-
-	}
+fast;in(t);
+    while(t--){
+    	
+    	
+    	}
 flush;
 return 0;
 }
