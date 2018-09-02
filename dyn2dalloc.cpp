c4618e5032907b874f28b530c8c1285814ac8edd
Ranjeet Kumar <34621600+rs119574@users.noreply.github.com>
2018-09-02 11:18:09 +0530

    shows how to declare n delete dyn2d

--- dyn2dalloc.cpp
+++ dyn2dalloc.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 int main(){
-	
 	int rows,cols;
 	cin>>rows>>cols;
 
 	int** matrix = new int*[rows];
 	for (int i = 0; i < rows; ++i)
 	    matrix[i] = new int[cols];
+	
+	
 	//to delete the matrix
-
 	
 	for (int i = 0; i < rows; ++i)
 	    delete [] matrix[i];
