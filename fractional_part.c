54de2a0ac6295c6ca1f68801852960731aa2cb91
Ranjeet Kumar <34621600+rs119574@users.noreply.github.com>
2018-09-02 10:41:14 +0530

    Delete fractional_part.c

--- fractional_part.c
+++
@@ -1,9 +0,0 @@
-main(){
- float f=254.73;
-
-    int integer = (int)f;
-//    float fractional = f-integer;
-    float fractional_part_in_integer = ((f*100)%100);
-
-    printf ("The value is: %d", fractional_part_in_integer);
-}
