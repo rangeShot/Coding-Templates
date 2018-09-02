0b40ecc2aca1168dc2c19dc95634c9de7c7963bf
rs119574 <rs119574@gmail.com>
2018-09-02 15:16:26 +0530

    GO n Rock

---
+++ format.cpp
@@ -0,0 +1,40 @@
+/*
+by:-  
+                     _ __ ___ 119574 
+                    | '__/ __| 
+                    | |  \__ \ 
+                    |_|  |___/ 
+                    
+
+*/ 
+#include <bits/stdc++.h>
+#define ll long long int
+
+#define pb push_back
+#define len(a) a.size()
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ignor cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
+#define flush fflush(stdin);fflush(stdout);
+#define rep(i,n) for(int i=0;i<n;i++)
+#define sq(a) (a)*(a)
+#define cb(a) (a)*(a)*(a)
+#define gcd(a,b) __gcd(a,b)
+#define cil(a,b) (((a-1)/b)+1)
+#define settedbits(a) __builtin_popcount(a)
+#define totalbits(a) ((int)log2(a)+1)
+#define sp " "
+
+const ll mod=1e9+7;
+const double pi=acos(-1);
+
+int main(){
+	fast;
+	int t;cin>>t;
+	while(t--){
+
+
+
+	}
+flush;
+return 0;
+}
