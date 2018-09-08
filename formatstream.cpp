868d87b6f3625647d6110a9cbbe272dfe38cfcb5
rs119574 <rs119574@gmail.com>
2018-09-08 10:58:20 +0530

    Ready to go

---
+++ formatstream.cpp
@@ -0,0 +1,48 @@
+/*
+by:-  
+                     _ __ ___ 119574 
+                    | '__/ __| 
+                    | |  \__ \ 
+                    |_|  |___/ 
+                    
+
+*/ 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#define pb push_back
+#define len(a) a.size()
+#define sq(a) (a)*(a)
+#define cb(a) (a)*(a)*(a)
+#define gcd(a,b) __gcd(a,b)
+#define cil(a,b) (((a-1)/b)+1)
+#define totalbits(a) ((int)log2(a)+1)
+#define settedbits(a) __builtin_popcount(a)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ignor cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define arr(a,n) in(n);ll a[n];rep(i,n)scanf("%lld",&a[i])
+#define flush fflush(stdin);fflush(stdout)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define init int t;scanf("%d",&t);while(t--)
+#define in(a) ll a;scanf("%lld",&a)
+#define pt printf
+#define sp " "
+
+const ll mod=1e9+7;
+const double pi=acos(-1);
+#define inf 0x3f3f3f3f
+
+int main(){
+fast;
+	ifstream in;
+	in.open("__inp.txt");
+	fstream out;
+	out.open("__opt.txt");
+    init{
+		
+	}
+    
+flush;
+return 0;
+}
