437e9cced6a9d0ade16a62d7a337963e1813abf8
Ranjeet Kumar <rs119574@gmail.com>
2019-01-05 17:45:57 +0530

    Update dyn2dalloc.cpp

--- dyn2dalloc.cpp
+++ dyn2dalloc.cpp
@@ -1,24 +1,49 @@
-#include<iostream>
+/*
+by:-  
+                     _ __ ___ 119574 
+                    | '__/ __| 
+                    | |  \__ \ 
+                    |_|  |___/ 
+                    
 
+*/ 
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-	int rows, cols;
-	cin>>rows>>cols;
 
-	//to declare the matrix
-	
-	int** matrix = new int*[rows];
-	for (int i = 0; i < rows; ++i)
-	    matrix[i] = new int[cols];
-	
-	
-	
-	
-	//to delete the matrix
-	
-	for (int i = 0; i < rows; ++i)
-	    delete [] matrix[i];
-	delete [] matrix;
- 
-return 0;
+int main(){
+    ios::sync_with_stdio(false);
+    int row, col;
+    cin>>row>>col;
+    
+    
+    
+    //method 1
+    vector <vector<int>> v1;
+    v1.resize(col,vector<int>(row));
+    
+    
+    
+    //method 2
+    vector <vector <int> > v2(row);
+    for(int i=0,cnt=1 ; i<row ; i++){
+        v2[i]=vector<int> (col);
+    }
+    
+    
+    
+    //method 3
+    int** v3 = new int*[rows];
+	for (int i = 0; i < rows; ++i){
+	    v3[i] = new int[cols];
+	}
+	    
+
+    
+    for (int i=0;i<rows;++i){
+	    delete [] v3[i];
+    }
+	delete [] v3;
+    
+    
+    return 0;
 }
