d40575173df4159941c602bbfaf7084f2c204230
Ranjeet Kumar <rs119574@gmail.com>
2019-07-01 01:45:48 +0530

    Update format.java

--- Basic_Template/format.java
+++ Basic_Template/format.java
@@ -11,11 +11,16 @@ by:-
 import java.io.*;
 import java.math.*;
 import java.lang.*;
-import java.security.*;
 import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
+import java.security.*;
 import java.util.regex.*;
+import java.util.Scanner; 
+import java.io.IOException; 
+import java.util.concurrent.*;
+import java.io.BufferedReader; 
+import java.io.InputStreamReader; 
+import java.util.StringTokenizer; 
 
 /*
 Set<Integer> st = new HashSet<Integer>();st.add();
@@ -26,16 +31,75 @@ Collections.reverse(Arrays.asList(a));
 
 */
 
-class A{
-	public static void main (String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        long t=scan.nextLong();
-        while(t-->0){
-        	
 
-        	
-        }
-        
-		
-	}
-}
+public class Main { 
+	static class FastReader { 
+		BufferedReader br; 
+		StringTokenizer st; 
+
+		public FastReader() { 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() { 
+			while (st == null || !st.hasMoreElements()) { 
+				try{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e){ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt(){ 
+			return Integer.parseInt(next()); 
+		} 
+
+		long nextLong(){ 
+			return Long.parseLong(next()); 
+		} 
+
+		double nextDouble(){ 
+			return Double.parseDouble(next()); 
+		} 
+
+		String nextLine(){ 
+			String str = ""; 
+			try
+			{ 
+				str = br.readLine(); 
+			} 
+			catch (IOException e) 
+			{ 
+				e.printStackTrace(); 
+			} 
+			return str; 
+		} 
+	} 
+
+	public static void main(String[] args) throws IOException { 
+		FastReader in=new FastReader(); 
+		Long t=1;
+		t=in.nextLong();
+		while(t--){
+		    //code
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+}}} 
