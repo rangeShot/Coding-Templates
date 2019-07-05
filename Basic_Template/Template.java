e39f5464af299282009b6aa2bf729a380181a10d
rs119574 <rs119574@gmail.com>
2019-07-05 18:34:05 +0530

    commited

---
+++ Basic_Template/Template.java
@@ -0,0 +1,104 @@
+/*
+by:-  
+                     _ __ ___ 119574 
+                    | '__/ __| 
+                    | |  \__ \ 
+                    |_|  |___/ 
+                    
+
+*/ 
+
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+import java.text.*;
+import java.util.*;
+// import java.security.*;
+// import java.util.regex.*;
+// import java.util.Scanner; 
+// import java.io.IOException; 
+// import java.util.concurrent.*;
+// import java.io.BufferedReader; 
+// import java.io.InputStreamReader; 
+// import java.util.StringTokenizer; 
+
+public class Template { 
+	static class FastReader { 
+		BufferedReader br; 
+		StringTokenizer st; 
+
+		public FastReader() { 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() { 
+			while (st == null || !st.hasMoreElements()) { 
+				try{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e){ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt(){ 
+			return Integer.parseInt(next()); 
+		} 
+
+		long nextLong(){ 
+			return Long.parseLong(next()); 
+		} 
+
+		double nextDouble(){ 
+			return Double.parseDouble(next()); 
+		} 
+
+		String nextLine(){ 
+			String str = ""; 
+			try{ 
+				str = br.readLine(); 
+			} 
+			catch (IOException e){ 
+				e.printStackTrace(); 
+			} 
+			return str; 
+		} 
+	} 
+
+
+/*
+Set<Integer> st = new HashSet<Integer>();st.add();
+Map<String,Integer>hm=new HashMap< String,Integer>(); 
+hm.put("a", new Integer(100));
+Set< Map.Entry< String,Integer> > st = hm.entrySet();
+for (Map.Entry< String,Integer> me:st) System.out.print(me.getKey()+":"+me.getValue()); 
+long []a=new long[26];
+Arrays.sort(a);
+Collections.reverse(Arrays.asList(a)); 
+
+
+*/
+
+public static void main(String[] args) throws IOException { 
+	FastReader in=new FastReader(); 
+	Long t=1L;
+// 	t=in.nextLong();
+	while(t-->0){
+	    //code
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+    }
+}} 
+
